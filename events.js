5ce77e64f80464348fa4740520cbd170777ac4b8
anant agnihotri <anant agnihotri>
2017-10-08 00:10:04 +0530

    added modal rules to events

---
+++ events.js
@@ -0,0 +1,55 @@
+$("#footballDiv").click(function () {
+    $('.modalDialog1').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog1').css('opacity', '0').css('pointer-events', 'none');
+
+  });
+
+
+  $("#basketballDiv").click(function () {
+    $('.modalDialog2').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog2').css('opacity', '0').css('pointer-events', 'none');
+
+  });
+
+
+  $("#cricketDiv").click(function () {
+    $('.modalDialog3').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog3').css('opacity', '0').css('pointer-events', 'none');
+
+  });
+
+  $("#badmintonDiv").click(function () {
+    $('.modalDialog4').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog4').css('opacity', '0').css('pointer-events', 'none');
+
+  });
+
+  $("#volleyballDiv").click(function () {
+    $('.modalDialog5').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog5').css('opacity', '0').css('pointer-events', 'none');
+
+  });
+  $("#chessDiv").click(function () {
+    $('.modalDialog6').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog6').css('opacity', '0').css('pointer-events', 'none');
+
+  });
+  $("#tabletennisDiv").click(function () {
+    $('.modalDialog7').css('opacity', '1').css('pointer-events', 'auto');
+  });
+  $('.close').click(function () {
+    $('.modalDialog7').css('opacity', '0').css('pointer-events', 'none');
+
+  });
