83d815810a9470cf096bae94e9e85c02cfeff232
anant agnihotri <anant agnihotri>
2017-10-08 15:08:09 +0530

    Added gallery to navbar

--- cont_res/contact.js
+++ cont_res/contact.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
-    
-      var active1 = false;
-      var active2 = false;
-      var active3 = false;
-      var active4 = false;
-      var active5 = false;
-    let c= 0;
-    
-        $('.topleftMenu').on('click', function() {
+  
+    var active1 = false;
+    var active2 = false;
+    var active3 = false;
+    var active4 = false;
+    var active5 = false;
+    var active6 = false;
+    let c = 0;
+  
+      $('.topLeftMenu').on('click', function() {
+      
+      if (!active1) $(this).find('.menu1').css({'background-color': '#fbcfie', 'transform': 'translate(-20px,135px)'});
+      else $(this).find('.menu1').css({'background-color': '#fbcf1e', 'transform': 'none'}); 
+       if (!active2) $(this).find('.menu2').css({'background-color': '#fbcf1e', 'transform': 'translate(45px,125px)'});
+      else $(this).find('.menu2').css({'background-color': '#fbcf1e', 'transform': 'none'});
+        if (!active3) $(this).find('.menu3').css({'background-color': '#fbcf1e', 'transform': 'translate(103px,100px)'});
+      else $(this).find('.menu3').css({'background-color': '#fbcf1e', 'transform': 'none'});
+        if (!active4) $(this).find('.menu4').css({'background-color': '#fbcf1e', 'transform': 'translate(145px,-23px)'});
+      else $(this).find('.menu4').css({'background-color': '#fbcf1e', 'transform': 'none'});
+      if (!active5) $(this).find('.menu5').css({'background-color': '#fbcf1e', 'transform': 'translate(60px,48px)'});
+      else $(this).find('.menu5').css({'background-color': '#fbcf1e', 'transform': 'none'});
+      if (!active6) $(this).find('.menu6').css({'background-color': '#fbcf1e', 'transform': 'translate(133px,40px)'});
+      else $(this).find('.menu6').css({'background-color': '#fbcf1e', 'transform': 'none'});
+      active1 = !active1;
+      active2 = !active2;
+      active3 = !active3;
+      active4 = !active4;
+      active5 = !active5;
+      active6 = !active6;
         
-        if (!active1) $(this).find('.menu1').css({'background-color': '#fbcfie', 'transform': 'translate(0px,130px)'});
-        else $(this).find('.menu1').css({'background-color': '#fbcf1e', 'transform': 'none'}); 
-         if (!active2) $(this).find('.menu2').css({'background-color': '#fbcf1e', 'transform': 'translate(65px,110px)'});
-        else $(this).find('.menu2').css({'background-color': '#fbcf1e', 'transform': 'none'});
-          if (!active3) $(this).find('.menu3').css({'background-color': '#fbcf1e', 'transform': 'translate(115px,65px)'});
-        else $(this).find('.menu3').css({'background-color': '#fbcf1e', 'transform': 'none'});
-          if (!active4) $(this).find('.menu4').css({'background-color': '#fbcf1e', 'transform': 'translate(130px,0px)'});
-        else $(this).find('.menu4').css({'background-color': '#fbcf1e', 'transform': 'none'});
-        if (!active5) $(this).find('.menu5').css({'background-color': '#fbcf1e', 'transform': 'translate(49px,45px)'});
-        else $(this).find('.menu5').css({'background-color': '#fbcf1e', 'transform': 'none'});
-        active1 = !active1;
-        active2 = !active2;
-        active3 = !active3;
-        active4 = !active4;
-        active5 = !active5;
-          
-        });
+      });
         $('.flip-container1').delay(2800).queue(function(){
             $(this).addClass('flipped')
         });
